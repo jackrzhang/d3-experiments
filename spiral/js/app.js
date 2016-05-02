7f9b4609090d21dd8f6fb97b15a139b1018c0a6b
Jack Zhang <15jzhang@gmail.com>
2016-05-02 00:39:50 -0700

    Create spiral

--- spiral/js/app.js
+++ spiral/js/app.js
@@ -1,15 +1,15 @@
-const expandDuration = 1500;
-const spiralRadius = 170;
-const spiralRate = 2;
-const startRadius = 5;
+const interval = 5;
+const expandDuration = 2500;
+const spiralRadius = window.innerWidth;
+const spiralRate = 3;
+const startRadius = 30;
 const endRadius = 0;
-const startWidth = '8px';
-const endWidth = '5px';
+const startWidth = '5px';
+const endWidth = '10px';
 
 const area = d3.select('body')
   .append('div')
   .attr('class', 'd3-area');
-
 
 let color;
 let hslIndex = 0;
@@ -34,7 +34,7 @@ let createCircle = (mouse) => {
     .transition()
     .ease(Math.sqrt)
     .duration(expandDuration)
-    .style('opacity', 0)
+    .style('opacity', .65)
     .style('top', String(mouse[1] - endRadius) + 'px')
     .style('left', String(mouse[0] - endRadius) + 'px')
     .style('width',  String(endRadius * 2) + 'px')
@@ -43,9 +43,6 @@ let createCircle = (mouse) => {
     .remove();
 }
 
-// mouse move / touch move event listening
-let mouse;
-area.on("mousemove", function() { 
-  mouse = d3.mouse(this); 
-  createCircle(mouse);
-});
+setInterval(() => {
+  createCircle([window.innerWidth / 2, window.innerHeight / 2]);
+}, interval);
